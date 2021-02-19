b333bbc5eb96ac37a858bc2f760bf5ed2ba30e87
Byungwook Yoon <byung90@gmail.com>
2021-02-18 22:01:06 -0500

    complete

---
+++ Solved/assets/js/script.js
@@ -0,0 +1,17 @@
+var container = document.querySelector(".container");
+
+container.addEventListener("click", function (event) {
+  var element = event.target;
+  // TODO: Complete function
+  if (element.matches(".box")) {
+    var state = element.getAttribute("data-state");
+    if (state === "hidden") {
+      element.setAttribute("data-state", "visible");
+      element.textContent = element.getAttribute("data-number");
+    }
+    else {
+      element.setAttribute("data-state", "hidden");
+      element.textContent = "";
+    }
+  }
+});
